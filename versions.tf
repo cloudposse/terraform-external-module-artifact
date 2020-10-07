3b2edaff29327affa4c8a8827f5f0a51d29b4df0
Nuru <Nuru@users.noreply.github.com>
2020-10-06 17:52:13 -0700

    Support AWS 3.x (#9)

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.12.0"
 
   required_providers {
-    aws      = "~> 2.0"
-    template = "~> 2.0"
-    local    = "~> 1.3"
-    external = "~> 1.2"
+    aws      = ">= 2.0"
+    template = ">= 2.0"
+    local    = ">= 1.3"
+    external = ">= 1.2"
   }
 }
